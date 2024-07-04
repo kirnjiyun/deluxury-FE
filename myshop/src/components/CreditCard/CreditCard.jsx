9a03938dee74132581bb57b2f1400096de7c762f
kirnjiyun <kimjiyunee@naver.com>
2024-07-04 23:57:45 +0900

    충돌 해결

--- myshop/src/components/CreditCard/CreditCard.jsx
+++ myshop/src/components/CreditCard/CreditCard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Cards from "react-credit-cards";
-import "react-credit-cards/es/styles-compiled.css";
 
 export default class PaymentForm extends React.Component {
     state = {
